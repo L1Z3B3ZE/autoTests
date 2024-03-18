6219e0e79d99b8cc2a99be392ecdcabe741c82ae
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-18 18:46:16 +0700

    авторизация с существующими данными

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('loginTest', () => {
+  it('Existent authorization test', () => {
+    cy.fixture('loginTest').then(data => {
+      cy.log("Переход на страницу авторизации")
+      cy.visit('https://dev.profteam.su/login')
+
+      cy.log("Ввод существующего логина")
+      cy.get('input[class="form-input--text form-input"]').type(data.login)
+
+      cy.log('Ввод существующего пароля')
+      cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+      cy.log("Клик по кнопке 'Войти' ")
+      cy.get('.button__background-color-green').click()
+
+    })
   })
 })
