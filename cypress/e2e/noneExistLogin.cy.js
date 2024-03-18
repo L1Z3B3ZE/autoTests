d075257e28adbe84fee5953a17e05e2edf88fb4a
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-18 18:49:20 +0700

    авторизация с несуществующим логином

---
+++ cypress/e2e/noneExistLogin.cy.js
@@ -0,0 +1,23 @@
+describe('loginTest', () => {
+    it('None-existent login test', () => {
+        cy.fixture('loginTest').then(data => {
+            cy.log("Переход на страницу авторизации")
+            cy.visit('https://dev.profteam.su/login')
+
+            cy.log("Ввод несуществующего логина")
+            cy.get('input[class="form-input--text form-input"]').type(data.none_existent_login)
+
+            cy.log('Ввод существующего пароля')
+            cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+
+            cy.log("Клик по кнопке 'Войти' ")
+            cy.get('.button__background-color-green').click()
+
+            cy.wait(1000)
+
+            cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
+        })
+    })
+})
