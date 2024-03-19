c4f1383637c07f41aee173a73aec2b6991c03c7c
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-19 19:12:42 +0700

    добавление регистрации с неподходящей фамилией

--- cypress/e2e/Tests.cy.js
+++ cypress/e2e/Tests.cy.js
@@ -13,7 +13,6 @@ describe('authorizationTest', () => {
       cy.log("Клик по кнопке 'Войти' ")
       cy.get('.button__background-color-green').click()
 
-      //cy.wait(1000)
     })
   })
 
@@ -165,6 +164,37 @@ describe('registerTest', () => {
 
       cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
       cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+    })
+  })
+
+  it('None-existent register surname test', () => {
+    cy.fixture('registerTest').then(data => {
+      cy.log("Переход на страницу регистрации")
+      cy.visit(data.main_url)
+
+      cy.log("Ввод логина")
+      cy.get('div[class="form__labels"] input:first').type(data.login)
+
+      cy.log("Ввод почты")
+      cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+      cy.log('Ввод пароля')
+      cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.password)
+
+      cy.log('Ввод повтора пароля')
+      cy.get('div[class="form__labels"] div:first div:nth-child(4) input').type(data.confirmPasswd)
+
+      cy.log("Клик по кнопке 'Далее' ")
+      cy.get('div[class="form__buttons"] div:last button[type="submit"]').click()
+
+      cy.log("Ввод неподходящей фамилии")
+      cy.get('div[class="form__labels"] div:nth-child(2) div:nth-child(1) div input').type(data.none_existent_surname)
+
+      cy.wait(1000)
+
+      cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+
     })
   })
 })
