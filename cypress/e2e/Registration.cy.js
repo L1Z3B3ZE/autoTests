3ee97cf181e1ea7d84de5225309f57eeddfc1aa5
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-19 14:53:20 +0700

    добавление регистрации с неподходящим паролем

--- cypress/e2e/Registration.cy.js
+++ cypress/e2e/Registration.cy.js
@@ -1,5 +1,5 @@
 describe('registerTest', () => {
-    it('None-existent register login', () =>{
+    it('None-existent register login test', () =>{
         cy.fixture('registerTest').then(data => {
             cy.log("Переход на страницу регистрации")
             cy.visit(data.main_url)
@@ -14,7 +14,7 @@ describe('registerTest', () => {
         })
     })
 
-    it('None-existent register email', () =>{
+    it('None-existent register email test', () =>{
         cy.fixture('registerTest').then(data => {
             cy.log("Переход на страницу регистрации")
             cy.visit(data.main_url)
@@ -24,6 +24,27 @@ describe('registerTest', () => {
 
             cy.log("Ввод неподходящей электронной почты")
             cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.none_existent_email)
+
+            cy.wait(1000)
+
+            cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+            cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+        })
+    })
+
+    it('None-existent register password test', () => {
+        cy.fixture('registerTest').then(data => {
+            cy.log("Переход на страницу регистрации")
+            cy.visit(data.main_url)
+
+            cy.log("Ввод логина")
+            cy.get('div[class="form__labels"] input:first').type(data.login)
+
+            cy.log("Ввод почты")
+            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+            cy.log('Ввод неподходящего пароля')
+            cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.none_existent_passwd)
 
             cy.wait(1000)
 
--- cypress/e2e/Tests.cy.js
+++ cypress/e2e/Tests.cy.js
@@ -13,7 +13,7 @@ describe('authorizationTest', () => {
       cy.log("Клик по кнопке 'Войти' ")
       cy.get('.button__background-color-green').click()
 
-      cy.wait(3000)
+      cy.wait(1000)
     })
   })
 
@@ -32,7 +32,7 @@ describe('authorizationTest', () => {
       cy.log("Клик по кнопке 'Войти' ")
       cy.get('.button__background-color-green').click()
 
-      cy.wait(2000)
+      cy.wait(1000)
 
       cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
       cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
@@ -90,7 +90,7 @@ describe('authorizationTest', () => {
 
 describe('registerTest', () => {
 
-  it('None-existent register login', () =>{
+  it('None-existent register login test', () =>{
     cy.fixture('registerTest').then(data => {
       cy.log("Переход на страницу регистрации")
       cy.visit(data.main_url)
@@ -105,7 +105,7 @@ describe('registerTest', () => {
     })
   })
 
-  it('None-existent register email', () =>{
+  it('None-existent register email test', () =>{
     cy.fixture('registerTest').then(data => {
       cy.log("Переход на страницу регистрации")
       cy.visit(data.main_url)
@@ -115,6 +115,27 @@ describe('registerTest', () => {
 
       cy.log("Ввод неподходящей электронной почты")
       cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.none_existent_email)
+
+      cy.wait(1000)
+
+      cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+      cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+    })
+  })
+
+  it('None-existent register password test', () => {
+    cy.fixture('registerTest').then(data => {
+      cy.log("Переход на страницу регистрации")
+      cy.visit(data.main_url)
+
+      cy.log("Ввод логина")
+      cy.get('div[class="form__labels"] input:first').type(data.login)
+
+      cy.log("Ввод почты")
+      cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.email)
+
+      cy.log('Ввод неподходящего пароля')
+      cy.get('div[class="form__labels"] div:first div:nth-child(3) input').type(data.none_existent_passwd)
 
       cy.wait(1000)
 
