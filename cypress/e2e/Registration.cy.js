c9cd62e47e391f08a7029510ec75252ec4e9190c
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-19 13:28:09 +0700

    добавление регистрации с неподходящей электронной почтой

--- cypress/e2e/Registration.cy.js
+++ cypress/e2e/Registration.cy.js
@@ -1,5 +1,4 @@
 describe('registerTest', () => {
-
     it('None-existent register login', () =>{
         cy.fixture('registerTest').then(data => {
             cy.log("Переход на страницу регистрации")
@@ -7,6 +6,26 @@ describe('registerTest', () => {
 
             cy.log("Ввод неподходящего логина")
             cy.get('div[class="form__labels"] input:first').type(data.none_existent_login)
+
+            cy.wait(1000)
+
+            cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+            cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
+        })
+    })
+
+    it('None-existent register email', () =>{
+        cy.fixture('registerTest').then(data => {
+            cy.log("Переход на страницу регистрации")
+            cy.visit(data.main_url)
+
+            cy.log("Ввод логина")
+            cy.get('div[class="form__labels"] input:first').type(data.login)
+
+            cy.log("Ввод неподходящей электронной почты")
+            cy.get('div[class="form__labels"] div:first div:nth-child(2) input').type(data.none_existent_email)
+
+            cy.wait(1000)
 
             cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
             cy.get('div[class="form-error form-error-- form-error-- form-error--"]').should('exist')
