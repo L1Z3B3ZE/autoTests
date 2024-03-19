6f43e862a568981cabb52de9d08a8221e3e0e467
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-20 03:01:39 +0700

    добавление отклика на потребность студентом

--- cypress/e2e/requirement.cy.js
+++ cypress/e2e/requirement.cy.js
@@ -151,3 +151,34 @@ describe('requirementTest', () => {
     })
 })
 
+
+describe('studentRequirementTest', () => {
+    it('responding to a need', () => {
+        cy.fixture('requirement').then(data => {
+            cy.log("Переход на страницу авторизации")
+            cy.visit(data.main_url)
+
+            cy.log("Ввод существующего логина")
+            cy.get('input[class="form-input--text form-input"]').type(data.student_login)
+
+            cy.log('Ввод существующего пароля')
+            cy.get('input[class="form-input--password form-input"]').type(data.student_password)
+
+            cy.log("Клик по кнопке 'Войти' ")
+            cy.get('.button__background-color-green').click()
+            cy.wait(2000)
+
+            cy.log("Выбор пункта меню 'Потребности' ")
+            cy.get('nav[class="header__nav"] a:nth-child(2)').click()
+            cy.wait(2000)
+
+            cy.get('.need-item--approved')
+                .find('.button__background-color-green') // Находим кнопку отклика
+                .not('[disabled]') // Фильтруем только активные кнопки
+                .first() // Берем первую активную кнопку
+                .click() // Нажимаем на кнопку
+
+        })
+    })
+})
+
