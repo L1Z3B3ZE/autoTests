ae38a8869b4a5993fd85246341c9f7c8093b9fac
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-20 04:19:51 +0700

    добавление просмотра уведомлений пользователя

--- cypress/e2e/notification.cy.js
+++ cypress/e2e/notification.cy.js
@@ -1,5 +1,5 @@
 describe('notificationTest', () => {
-    it('viewNotification', () => {
+    it('readNotification', () => {
         cy.fixture('notification').then(data => {
             cy.log("Переход на страницу авторизации")
             cy.visit(data.main_url)
@@ -19,6 +19,29 @@ describe('notificationTest', () => {
 
             cy.log("Клик по кнопке 'Прочитать все' ")
             cy.get('.notification-title > :nth-child(3)').click()
+        })
+    })
+
+    it('viewNotification', () => {
+        cy.fixture('notification').then(data => {
+            cy.log("Переход на страницу авторизации")
+            cy.visit(data.main_url)
+
+            cy.log("Ввод существующего логина")
+            cy.get('input[class="form-input--text form-input"]').type(data.login)
+
+            cy.log('Ввод существующего пароля')
+            cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+            cy.log("Клик по кнопке 'Войти' ")
+            cy.get('.button__background-color-green').click()
+
+            cy.log("Выбор пункта меню 'Уведомления' ")
+            cy.get('nav[class="header__nav"] a:nth-child(4)').click()
+            cy.wait(2000)
+
+            cy.log("Клик по кнопке 'Просмотреть' ")
+            cy.get('div.notification-list-wrapper').first().find('button[type="submit"]').click()
 
         })
     })
