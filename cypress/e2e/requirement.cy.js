5848267cf9be2f20ac66f0c33d76b78f8fe5091f
L1Z3B3ZE <mr.lisa.lisa.lisa@mail.ru>
2024-03-20 09:19:51 +0700

    добавление превышения значений полей создания потребности

--- cypress/e2e/requirement.cy.js
+++ cypress/e2e/requirement.cy.js
@@ -149,6 +149,52 @@ describe('requirementTest', () => {
 
         })
     })
+
+    it('invalid requirement field', () => {
+        cy.fixture('requirement').then(data => {
+            cy.log("Переход на страницу авторизации")
+            cy.visit(data.main_url)
+
+            cy.log("Ввод существующего логина")
+            cy.get('input[class="form-input--text form-input"]').type(data.login)
+
+            cy.log('Ввод существующего пароля')
+            cy.get('input[class="form-input--password form-input"]').type(data.password)
+
+            cy.log("Клик по кнопке 'Войти' ")
+            cy.get('.button__background-color-green').click()
+
+            cy.log("Выбор пункта меню 'Потребности' ")
+            cy.get('div[class="page-navigation"] div:first div:nth-child(6) p').click()
+            cy.wait(2000)
+
+            cy.log("Клик по кнопке 'Создать потребность' ")
+            cy.get('div[class="needs-block__content"] div:first button[type="submit"]').click()
+
+            cy.log("Ввод названия потребности")
+            cy.get('.desktop-modal .labels .label:first div input').type(data.invalid_requirement)
+            cy.wait(1000)
+
+            cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+            cy.get('.desktop-modal .labels .label:first div:nth-child(2) span').should('exist')
+
+            cy.log("Ввод обязанностей")
+            cy.get('.desktop-modal .labels .label:nth-child(3) div textarea').type(data.invalid_duties)
+            cy.wait(1000)
+
+            cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+            cy.get('.desktop-modal .labels .label:nth-child(3) div:nth-child(2)').should('exist')
+
+            cy.log("Ввод требований")
+            cy.get('.desktop-modal .labels .label:nth-child(4) div textarea').type(data.invalid_requests)
+            cy.wait(1000)
+
+            cy.log("Проверка вывода сообщения, сигнализирующего об ошибке")
+            cy.get('.desktop-modal .labels .label:nth-child(4) div:nth-child(2)').should('exist')
+
+        })
+    })
+
 })
 
 
